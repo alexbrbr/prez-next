59e9d4a15e87a42e27e396ef46d48f93b7a568f2
BARBIER, Alexandre (ATY) <alexandre.barbier@infopro-digital.com>
2018-09-24 14:51:11 +0200

    use React.Component and getInitialProps with fetch

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
 // pages/index.js
+require('isomorphic-fetch');
 import Link from 'next/link'
+import React from 'react'
 
-export default () => (
-  <div>
-    <p>
-      👋 la bouffe front 😻
-    </p>
-    <Link prefetch href="/cow">
-      Meuh
-    </Link>{' '}
-  </div>
-)
+export default class extends React.Component {
+  static async getInitialProps({ req }) {
+    const res = await fetch('https://api.github.com/repos/zeit/next.js')
+    const json = await res.json()
+    return { stars: json.stargazers_count }
+  }
+
+  render() {
+    const { stars } = this.props;
+    return (
+        <div>
+            <p>
+            👋 la bouffe front 😻
+            </p>
+            <p>
+            Next stars: {stars}
+            </p>
+            <Link prefetch href="/cow">
+              Meuh
+            </Link>{' '}
+        </div>
+    
+    )
+  }
+}
